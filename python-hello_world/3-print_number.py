f094bc37734a357ba5dba41586b6dceb3b897e70
andrewthonriem <a.alier@alustudent.com>
2025-06-28 06:11:53 -0700

    3

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-number = 5  # You can change this to test different values
-
-if isinstance(number, int):
-    if number > 0:
-        print(f"{number} is positive")
-    elif number == 0:
-        print(f"{number} is zero")
-    else:
-        print(f"{number} is negative")
-else:
-    print("Wrong type")
+number = 98
+print(f"{number} Battery street")
