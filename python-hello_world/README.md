990f3ace42629755e0ceee07da765bf8885aff10
andrewthonriem <a.alier@alustudent.com>
2025-07-19 02:34:45 -0700

    done

---
+++ python-hello_world/README.md
@@ -0,0 +1,1 @@
+a Folder that stores the python project task 1.
---
+++ python-hello_world/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+print("Best School")
+#!/usr/bin/python3
+print("Best School")
+#!/usr/bin/python3
+print("Best School")
+#!/usr/bin/python3
+print("Best School")
