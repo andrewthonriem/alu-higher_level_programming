444fbb926e1cc901b44913b3b3c879c1b53f7457
andrewthonriem <a.alier@alustudent.com>
2025-07-19 01:40:10 -0700

    8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:54] + str[54] + str[55:66] + str[66] + str[77:81] + str[81] + str[0:6])
+print(str[39:54] + str[54] + str[55:66] + str[66] + str[76:80] + str[80] + str[0:6])
 
